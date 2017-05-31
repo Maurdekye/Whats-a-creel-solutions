af1082c26128ca133a7640a5858282c389e6affc
Maurdekye <ncolaprete@gmail.com>
2017-05-30 20:32:27 -0400

    Added deciphering script for riddle 10

--- riddle_6_again.py
+++ riddle_6_again.py
@@ -4,7 +4,7 @@ mpmath.mp.dps = 20
 rt5 = mpmath.sqrt(5)
 overrt5 = 1 / rt5
 
-def fib(n):
+def fib(n): # maybe using the algebraic solution?
   return overrt5 * mpmath.phi ** n
 
-print(int(fib(1_000_000_000)))
+print(int(fib(1_000_000_000))) # still abominably slow
